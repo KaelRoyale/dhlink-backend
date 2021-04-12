efe509993aec8e43472ba0895a62ef34697414c6
Thanh Phong Pham <pham0226@flinders.edu.au>
2021-04-13 00:34:45 +0930

    put route

--- express/server.js
+++ express/server.js
@@ -9,12 +9,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const db = require('./db')
 
+//Controllers
+const ServiceController = require('../controllers/serviceController')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors(), helmet())
 app.use(bodyParser.json())
 
 const router = require('./routes/router')
+const apiPort = 8000
+app.listen(apiPort, () => {
 
+  console.log(`Server running on port ${apiPort}`)})
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -23,6 +29,12 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+
+router.get('/api/services', (req, res) => {
+  ServiceController.getAll
+})
+
 app.use('/api', router)
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
