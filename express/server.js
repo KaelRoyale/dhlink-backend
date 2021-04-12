9d8f3dc327250bc035ea00977881a2506342de29
Thanh Phong Pham <pham0226@flinders.edu.au>
2021-04-12 23:18:41 +0930

    update server

--- express/server.js
+++ express/server.js
@@ -1,17 +1,20 @@
 'use strict';
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors')
+const helmet = require('helmet')
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const db = require('./db')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors(), helmet())
 app.use(bodyParser.json())
 
-const routes = require('./routes/router')
-const router = express.Router();
+const router = require('./routes/router')
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -20,7 +23,7 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
-app.use('/api', Router)
+app.get('/api', router)
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
